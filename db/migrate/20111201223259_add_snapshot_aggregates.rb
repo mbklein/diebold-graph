cc3e051fe8612212bc957604defcd1672b063958
Michael Klein <mbklein@gmail.com>
2011-12-01 15:08:52 -0800

    Change AddSnapshotAggregates to an explicitly up/down migration

--- db/migrate/20111201223259_add_snapshot_aggregates.rb
+++ db/migrate/20111201223259_add_snapshot_aggregates.rb
@@ -1,8 +1,14 @@
 class AddSnapshotAggregates < ActiveRecord::Migration
-  def change
+  def up
     add_column(:snapshots, :deviation_from_median, :integer)
     add_column(:snapshots, :deviation_from_mean, :integer)
     add_column(:snapshots, :percentage_of_max, :float)
     Snapshot.find(:all).each { |snap| snap.update_aggregate_attributes }
+  end
+  
+  def down
+    remove_column(:snapshots, :deviation_from_median)
+    remove_column(:snapshots, :deviation_from_mean)
+    remove_column(:snapshots, :percentage_of_max)
   end
 end
