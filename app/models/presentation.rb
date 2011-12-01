1c522503a2d04a55d2eff2d964b1491ca05d3e92
Michael Klein <mbklein@gmail.com>
2011-12-01 11:32:33 -0800

    Round off the snapshot time to the nearest second

--- app/models/presentation.rb
+++ app/models/presentation.rb
@@ -4,12 +4,13 @@ class Presentation < ActiveRecord::Base
   @@client = RestClient::Resource.new("http://vote.code4lib.org/election/results/")
   def self.snapshot(election_id)
     results = JSON.parse(@@client[election_id].get({:accept => :json}))
+    snap_time = Time.now
     results.each do |result|
       pres = Presentation.find_or_create_by_id(result['id'])
       if pres.election_id.nil?
         pres.update_attributes :election_id => election_id, :title => result['title'], :description => result['description']
       end
-      pres.snapshots.create(:at => Time.now, :score => result['score'])
+      pres.snapshots.create(:at => snap_time, :score => result['score'])
     end
   end
   
