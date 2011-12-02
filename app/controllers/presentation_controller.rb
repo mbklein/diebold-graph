47a1c4a16f34ec561a5585ca4e5499f198959b4e
Michael Klein <mbklein@gmail.com>
2011-12-02 12:25:02 -0800

    AJAX-y data change -- save zoom level on redraw, add "Top 20" link

--- app/controllers/presentation_controller.rb
+++ app/controllers/presentation_controller.rb
@@ -1,14 +1,13 @@
 class PresentationController < ApplicationController
-
+  include PresentationHelper
+  
   def view
     @by = params[:by] ? params[:by].to_sym : :score
     @presentations = Presentation.find_all_by_election_id params[:id]
     @presentations.sort! { |a,b| b.snapshots.last.send(@by) <=> a.snapshots.last.send(@by) }
     respond_to do |format|
       format.html 
-      format.json { 
-        render :json => @presentations.collect { |p| p.series }.to_json 
-      }
+      format.json { render :json => series_data }
     end
   end
 
--- app/helpers/presentation_helper.rb
+++ app/helpers/presentation_helper.rb
@@ -1,11 +1,19 @@
 module PresentationHelper
   
   def render_series_data
+    raw(series_data.to_json)
+  end
+  
+  def series_data
     series = @presentations.collect { |p| p.series(@by) }
     if params[:top]
-      series = series[0..params[:top].to_i-1]
+      cutoff = params[:top].to_i-1
+      while cutoff < series.length-1 and series[cutoff][:data].last.last == series[cutoff+1][:data].last.last
+        cutoff += 1
+      end
+      series = series[0..cutoff]
     end
-    raw(series.to_json)
+    series
   end
   
 end
